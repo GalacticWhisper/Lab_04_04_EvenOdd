7ce07121abea14fe58a7fcfca434ec50fb2d94fc
IanM <ianmarr@Ians-MacBook-Air.local>
2024-09-22 21:21:33 -0400

    Init Commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int evalNumber = 7;
+        int modVal = evalNumber % 2;
+            if (modVal == 0);
+                System.out.printf("The evalNumber value " + evalNumber + " is even.");
+    }
+}
